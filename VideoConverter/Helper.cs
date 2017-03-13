fdee0904fb2e243c809a6257abea1d677c7ba53a
Joel Pe <jope@freightcenter.com>
2017-03-12 21:00:35 -0400

    Add Video Converter

---
+++ VideoConverter/Helper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class Helper {
+
+    public static byte[] ConvertToBytes(string sourcePath) {
+        byte[] bytes = null;
+
+        using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+            using (System.IO.FileStream f = new System.IO.FileStream(sourcePath, System.IO.FileMode.Open, System.IO.FileAccess.Read)) {
+                f.CopyTo(ms);
+                bytes = ms.ToArray();
+            }
+        }
+
+        return bytes;
+    }
+
+    public static string ProcessVideo(string options) {
+        var output = string.Empty;
+        var ffmpegPath = ConfigurationManager.AppSettings["Component.Media:ffmpeg"];
+
+        try {
+            ProcessStartInfo info = new ProcessStartInfo(Path.Combine(ffmpegPath, "ffmpeg.exe"), options);
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+
+            Process proc = System.Diagnostics.Process.Start(info);
+            output = proc.StandardError.ReadToEnd();
+
+            proc.WaitForExit();
+            proc.Close();
+        } catch (Exception ex) { }
+
+        return output;
+    }
+
+    public static VideoConversionSettings GetVideoInfo(string sourcePath) {
+        if (string.IsNullOrEmpty(sourcePath)) {
+            return null;
+        }
+
+        var video = new VideoConversionSettings();
+
+        //set up the parameters for video info
+        string output = ProcessVideo(string.Format("-i {0}", sourcePath));
+
+        //get duration
+        Regex re = new Regex("[D|d]uration:.((\\d|:|\\.)*)");
+        Match m = re.Match(output);
+
+        if (m.Success) {
+            string duration = m.Groups[1].Value;
+            string[] timepieces = duration.Split(new char[] { ':', '.' });
+            if (timepieces.Length == 4) {
+                video.Duration = new TimeSpan(0, System.Convert.ToInt16(timepieces[0]), System.Convert.ToInt16(timepieces[1]), System.Convert.ToInt16(timepieces[2]), System.Convert.ToInt16(timepieces[3]));
+            }
+        }
+
+        //get audio bit rate
+        re = new Regex("[B|b]itrate:.((\\d|:)*)");
+        m = re.Match(output);
+        decimal kb = 0;
+        if (m.Success) {
+            decimal.TryParse(m.Groups[1].Value, out kb);
+        }
+
+        video.AudioBitrate = kb;
+
+        //get the audio format
+        re = new Regex("[A|a]udio:.*");
+        m = re.Match(output);
+        if (m.Success) {
+            video.AudioFormat = m.Value;
+        }
+
+        //get the video format
+        re = new Regex("[V|v]ideo:.*");
+        m = re.Match(output);
+        if (m.Success) {
+            video.VideoFormat = m.Value;
+        }
+
+        //get the video format
+        re = new Regex("(\\d{2,3})x(\\d{2,3})");
+        m = re.Match(output);
+        if (m.Success) {
+            int width = 0; int height = 0;
+            int.TryParse(m.Groups[1].Value, out width);
+            int.TryParse(m.Groups[2].Value, out height);
+
+            video.Height = height;
+            video.Width = width;
+        }
+
+        return video;
+    }
+
+}
---
+++ VideoConverter/VideoConversion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+public class VideoConversion {
+
+    public VideoConversion(VideoConversionSettings settings) {
+        this.Settings = settings;
+        this.Settings.ID = Guid.NewGuid();
+
+        //check if the resolution is provided otherwise we will use the original WxH
+        if (settings.Width == 0 && settings.Height == 0) {
+            if (File.Exists(settings.SourcePath)) {
+                var output = Helper.GetVideoInfo(settings.SourcePath);
+
+                settings.Height = output.Height;
+                settings.Width = output.Width;
+            } else {
+                //The file do not exists
+                //To do: log this error in to the database.                    
+            }
+        }
+    }
+
+    private VideoConversionSettings Settings { get; set; }
+
+    public void ToJpeg() {
+        var targetPath = System.IO.Path.Combine(Settings.TargetPath, string.Format("{0}.jpg", Settings.ID));
+
+        if (!System.IO.File.Exists(targetPath)) {
+            int secs = (int)Math.Round(TimeSpan.FromTicks(Settings.Duration.Ticks / 3).TotalSeconds, 0);
+
+            Helper.ProcessVideo(string.Format("-i {0} {1} -ss {2} -qscale:v 2 -vframes 1", Settings.SourcePath, targetPath, secs));
+        }
+    }
+
+    public void ToNewVideoFormat() {
+        var targetPath = System.IO.Path.Combine(Settings.TargetPath, string.Format("{0}.{1}", Settings.ID, Settings.VideoFormat));
+
+        if (!System.IO.File.Exists(targetPath)) {
+            var options = string.Format(@"-i {0} -vcodec libx264 -acodec aac -strict -2 -b:v {1}k -ab {2}k -s {3} -framerate {4} -y {5}",
+                                       Settings.SourcePath,
+                                       Settings.VideoBitrate,
+                                       Settings.AudioBitrate,
+                                       Settings.Resolution,
+                                       Settings.FrameRate,
+                                       targetPath);
+
+            Helper.ProcessVideo(options);
+        }
+    }
+
+    public VideoConversionSettings GetVideoInfo(string sourcePath) {
+        return Helper.GetVideoInfo(sourcePath);
+    }
+}
+
+public class VideoConversionSettings {
+    /// <summary>
+    /// Video Conversion Settings ID
+    /// </summary>
+    public virtual Guid ID { get; set; }
+
+    public string SourcePath { get; set; }
+
+    public string TargetPath { get; set; }
+
+    public TimeSpan Duration { get; set; }
+
+    public string AspectRatio { get; set; }
+
+    public int Height { get; set; }
+
+    public int Width { get; set; }
+
+    public string Resolution {
+        get {
+
+            return string.Format("{0}x{1}", Width, Height);
+        }
+    }
+
+    public decimal VideoBitrate { get; set; }
+
+    public string VideoFormat { get; set; }
+
+    public decimal FrameRate { get; set; }
+
+    public decimal AudioBitrate { get; set; }
+
+    public string AudioFormat { get; set; }
+}
+
